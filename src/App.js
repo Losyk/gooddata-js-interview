fd4fe461d868e59b00ed8f1c40973e3b36e9365a
Losyk <mira.los@gmail.com>
2020-08-02 11:46:26 +0200

    Implemented dropdowns for changing month and year.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 // Copyright (C) 2007-2019, GoodData(R) Corporation. All rights reserved.
 
 import React, { Component } from 'react';
+import Dropdown from 'react-dropdown';
 import '@gooddata/react-components/styles/css/main.css';
+import 'react-dropdown/style.css';
+import './App.css';
 
 import { ColumnChart } from '@gooddata/react-components';
 
@@ -9,16 +12,42 @@ const grossProfitMeasure = '/gdc/md/xms7ga4tf3g3nzucd8380o2bev8oeknp/obj/6877';
 const dateAttributeInMonths = '/gdc/md/xms7ga4tf3g3nzucd8380o2bev8oeknp/obj/2142';
 const dateAttribute = '/gdc/md/xms7ga4tf3g3nzucd8380o2bev8oeknp/obj/2180';
 
+const monthOptions = [ 'January', 'February', 'March', 'April', 'May', 'June', 'July',
+    'August', 'September', 'October', 'November', 'December' ];
+const yearOptions = [ '2015', '2016', '2017' ];
+
+const defaultMonth = monthOptions[0];
+const defaultYear = yearOptions[1];
+
 class App extends Component {
 
+    constructor(props) {
+	super(props)
+
+	this.state = {
+	    selectedMonth: defaultMonth,
+	    selectedYear: defaultYear
+	}
+    }
+
     getMonthFilter() {
+	// index of month (starting with 1)
+	const selectedMonth = monthOptions.indexOf(this.state.selectedMonth) + 1;
+	const selectedYear = this.state.selectedYear;
+
+	// https://stackoverflow.com/questions/222309/calculate-last-day-of-month-in-javascript
+	const lastDay = new Date(selectedYear, selectedMonth, 0);
+
+	const from = `${selectedYear}-${selectedMonth}-01`
+	const to = `${selectedYear}-${selectedMonth}-${lastDay.getDate()}`;
+
         return {
             absoluteDateFilter: {
                 dataSet: {
                     uri: dateAttribute
                 },
-                from: '2016-01-01',
-                to: '2016-01-31'
+                from: from,
+                to: to
             }
 
         }
@@ -54,23 +83,30 @@ class App extends Component {
         }
     }
 
-    renderDropdown() {
-        return (
-            <select defaultValue="1">
-                <option value="1">January</option>
-                <option value="2">February</option>
-                <option value="3">March</option>
-                <option value="4">April</option>
-                <option value="5">May</option>
-                <option value="6">June</option>
-                <option value="7">July</option>
-                <option value="8">August</option>
-                <option value="9">September</option>
-                <option value="10">October</option>
-                <option value="11">November</option>
-                <option value="12">December</option>
-            </select>
-        )
+    changeMonth(item) {
+	// very simple validation
+	if (monthOptions.indexOf(item.value) === -1) {
+	    alert('Invalid month option.');
+
+	    return;
+	}
+
+	this.setState(state => ({
+	    selectedMonth: item.value
+	}));
+    }
+
+    changeYear(item) {
+	// very simple validation
+	if (yearOptions.indexOf(item.value) === -1) {
+	    alert('Invalid year option.');
+
+	    return;
+	}
+
+	this.setState(state => ({
+	    selectedYear: item.value
+	}));
     }
 
     render() {
@@ -81,7 +117,22 @@ class App extends Component {
 
         return (
             <div className="App">
-                <h1>$ Gross Profit in month {this.renderDropdown()} 2016</h1>
+                <h1>$ Gross Profit in month
+		  <Dropdown
+		      className="App-dropdown"
+		      controlClassName="App-dropdown-control"
+		      options={monthOptions}
+		      onChange={this.changeMonth.bind(this)}
+		      value={this.state.selectedMonth}
+		  />
+		  <Dropdown
+		      className="App-dropdown"
+		      controlClassName="App-dropdown-control"
+		      options={yearOptions}
+		      onChange={this.changeYear.bind(this)}
+		      value={this.state.selectedYear}
+		  />
+		</h1>
                 <div>
                     <ColumnChart
                         measures={measures}
